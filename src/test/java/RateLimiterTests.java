b0f0d997119380a729d8a2c0079918de8851341a
aman@browserstack <aman.s@browserstack.com>
2024-03-18 12:41:51 +0530

    added Test for fixedWindowRateLimiter

---
+++ src/test/java/RateLimiterTests.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.rateLimiterExample.rateLimiters.api.RateLimiter;
+import org.rateLimiterExample.rateLimiters.constants.RateLimiterConstants;
+import org.rateLimiterExample.rateLimiters.factories.RateLimiterFactory;
+import org.rateLimiterExample.rateLimiters.models.Strategy;
+
+import java.util.Properties;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class RateLimiterTests {
+
+    @Test
+    public void testFixedWindowRateLimiter(){
+        Properties rateLimitConfig = new Properties();
+        rateLimitConfig.setProperty(RateLimiterConstants.MAX_REQUEST, "5");
+        rateLimitConfig.setProperty(RateLimiterConstants.TIME_WINDOW_MS,"10000");
+        RateLimiter<String> fixedWindowRateLimiter = RateLimiterFactory.<String>getRateLimiter(Strategy.FIXED_WINDOW, rateLimitConfig) ;
+
+        for(int i=0; i<=5; i++){
+
+            if (i<5){
+                assertTrue(fixedWindowRateLimiter.allow("user1"))  ;
+            }
+            else{
+                assertFalse(fixedWindowRateLimiter.allow("user1"));
+            }
+        }
+    }
+}
